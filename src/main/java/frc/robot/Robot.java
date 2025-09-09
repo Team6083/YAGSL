b79e9915d55f4044b1b2f071d1242faf22db6840
Hannahjjj97 <hannahjjj97@gmail.com>
2025-09-09 08:53:04 +0800

    feat: add swerveDriveTest code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,50 +4,101 @@
 
 package frc.robot;
 
+import java.io.File;
+import java.io.IOException;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import swervelib.SwerveController;
+import swervelib.SwerveDrive;
+import swervelib.SwerveDriveTest;
+import swervelib.parser.SwerveParser;
 
 /**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * The methods in this class are called automatically corresponding to each
+ * mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the
+ * package after creating
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
-  public Robot() {}
+  XboxController mainController = new XboxController(0);
+
+  File directory = new File("\\java\\frc\\robot\\YAGSLConfig\\swerve\\swervedrive.json");
+  SwerveParser swerveParser;
+  SwerveDrive swerveDrive;
+  SwerveController swerveController;
+
+  public Robot() {
+    try {
+      swerveParser = new SwerveParser(directory);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    swerveDrive = swerveParser.createSwerveDrive(4);
+    swerveController = swerveDrive.swerveController;
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if (mainController.getAButton()) {
+      SwerveDriveTest.centerModules(swerveDrive);
+    } else if (mainController.getLeftTriggerAxis() > 0.1) {
+      SwerveDriveTest.runAngleMotorsCharacterizationOnSimModules(
+          swerveDrive, mainController.getLeftTriggerAxis() * 12);
+    } else if (mainController.getRightTriggerAxis() > 0.1) {
+      SwerveDriveTest.runDriveMotorsCharacterizationOnSimModules(
+          swerveDrive, mainController.getRightTriggerAxis() * 12, true);
+    }
+
+    if (mainController.getBackButton()) {
+      swerveDrive.zeroGyro();
+    }
+  }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
